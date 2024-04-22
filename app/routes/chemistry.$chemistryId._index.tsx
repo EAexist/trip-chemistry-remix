38ea502c7a16953e6804c02b4257fab7f744922a
Pyo Hyeon <megatwins@kaist.ac.kr>
2024-04-22 19:44:21 +0900

    build: remove type:module from package.json

--- app/routes/chemistry.$chemistryId._index.tsx
+++ app/routes/chemistry.$chemistryId._index.tsx
@@ -3,7 +3,6 @@ import type { MetaFunction } from "@remix-run/node";
 export const meta: MetaFunction = ({ matches }) => {
   const parentMeta = matches[0].meta
     .filter((meta) => !("title" in meta) && !("og:description" in meta) );
-  console.log(parentMeta)
   return [
     ...parentMeta, 
     { title: "친구와 떠나는 여행을 준비해보세요 | 여행 타입 테스트" },
