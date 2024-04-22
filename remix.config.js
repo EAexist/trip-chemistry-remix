ca0b529dfbac9eece1c21428a83c4c419b7af991
Pyo Hyeon <megatwins@kaist.ac.kr>
2024-04-22 20:06:11 +0900

    build: update vite.config.ts to allow .cjs server module format

--- remix.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-  // TODO: when mui has esm support, remove this (default is esm)
-  // check it https://github.com/mui/material-ui/issues/30671
-  serverModuleFormat: 'cjs',
-};
--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix({ presets: [vercelPreset()] }), tsconfigPaths()],
+  plugins: [remix({ 
+    presets: [vercelPreset()], 
+    // TODO: when mui has esm support, remove this (default is esm)
+    // check it https://github.com/mui/material-ui/issues/30671
+    serverModuleFormat: 'cjs' 
+  }), tsconfigPaths()],
 });
